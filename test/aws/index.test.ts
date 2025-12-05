93610928a3c8119a937216b5803f78d4f7bf008a
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2025-12-05 23:45:07 +0900

    プロバイダのバージョンは無視する

--- test/aws/index.test.ts
+++ test/aws/index.test.ts
@@ -1,11 +1,14 @@
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider'
 import { Testing } from 'cdktf'
 import { describe, expect, it } from 'vitest'
 import { AWSStack } from '../../src/stacks/aws'
+import { forceIgnoreProviderVersion } from '../util'
 
 describe('AWS', () => {
   it('snapshot', () => {
     const app = Testing.app()
     const stack = new AWSStack(app, 'aws')
+    forceIgnoreProviderVersion(stack, AwsProvider)
     expect(Testing.synth(stack)).toMatchSnapshot()
   })
 })
