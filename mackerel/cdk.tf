f792f7d3a519b1e5fb46ad14b936781b1e887764
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2026-01-19 00:10:30 +0900

    default empty

--- mackerel/cdk.tf
+++ mackerel/cdk.tf
@@ -23,6 +23,7 @@ variable "MIRAKURUN_MONITOR_AUTHORIZATION_HEADER" {
   description = "Mirakurun monitor authorization header"
   type        = string
   sensitive   = true
+  default = ""
 }
 variable "WATCHDOGS_WEBHOOK_URL" {
   description = "watchdogs webhook url"
