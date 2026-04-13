9f8bb5b4e80db2c256ccdd23267f8676f3fd6379
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2026-04-13 09:27:19 +0900

    Litestream is over

--- mackerel/monitors.tf
+++ mackerel/monitors.tf
@@ -1,25 +1,8 @@
 resource "mackerel_monitor" "connectivity" {
   name = "connectivity"
-  # Keep this empty block: it selects the "connectivity" monitor type.
-  connectivity {
+  connectivity {}
+}
 
-  }
-}
-import {
-  to = mackerel_monitor.litestream_shadow_wal_size
-  id = "5o1EhqZ3XMS"
-}
-resource "mackerel_monitor" "litestream_shadow_wal_size" {
-  name = "Litestream Shadow WALサイズ肥大化"
-  memo = "レプリカ数が増えていなければ同期に失敗している"
-  query {
-    query    = "litestream_shadow_wal_size{service.name=\"litestream\"}"
-    legend   = "{{service.instance.id}}: {{db}}"
-    operator = ">"
-    warning  = "25000000"
-    critical = "50000000"
-  }
-}
 resource "mackerel_monitor" "materia_crashloopbackoff" {
   name = "Materia CrashLoopBackOff"
   query {
@@ -29,6 +12,7 @@ resource "mackerel_monitor" "materia_crashloopbackoff" {
     critical = "0"
   }
 }
+
 resource "mackerel_monitor" "materia_imagepull_error" {
   name = "Materia ImagePullエラー"
   query {
